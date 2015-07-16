f129fc953a7554c1f03a1e4ccffac59f682e3dd9
Colin Coltrera <c.coltrera@gmail.com>
2015-07-15 17:42:53 -0700

    Fix PUT so it complies with HTTP methods reading.

--- test/server-test.js
+++ test/server-test.js
@@ -16,7 +16,7 @@ describe("Persistant Server", function() {
     "birthDate" : 01011990,
     "email" : "c.colt@gmail.com",
     "emailconfirmed" : true
-  }
+  };
   var marcData = {
     "username" : "marccolt",
     "name" : "Marc Colt",
@@ -37,7 +37,7 @@ describe("Persistant Server", function() {
     "birthDate" : 08171955,
     "email" : "a.buch@gmail.com",
     "emailconfirmed" : true
-  }
+  };
   //Define serverInstance outside of before() scope so that it can be closed later.
   var serverInstance;
   //Start server
@@ -73,22 +73,27 @@ describe("Persistant Server", function() {
       done();
     });
   });
+  after(function(done) {
+    fs.unlink("./data/garrettdeick.json", function(err) {
+      done();
+    });
+  });
 
   describe("GET request", function() {
-    it("should respond to GET at /api/user/username for non-existing user with 404 status code", function(done) {
+    it("should respond to GET at /api/users/username for non-existing user with 404 status code", function(done) {
       chai.request("http://localhost:8080")
-        .get("/api/user/erincolt")
+        .get("/api/users/erincolt")
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(404);
           done();
         });
     });
-    it("should respond to GET at /api/user/username for existing user with JSON file", function(done) {
+    it("should respond to GET at /api/users/username for existing user with JSON file", function(done) {
       chai.request("http://localhost:8080")
-        .get("/api/user/colincolt")
+        .get("/api/users/colincolt")
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(200);
           expect(res).to.be.json;
           done();
@@ -104,23 +109,23 @@ describe("Persistant Server", function() {
     });
     it("should respond with 409 (conflict) on POST where resource already exists", function(done) {
       chai.request("http://localhost:8080")
-        .post("/api/user/colincolt")
+        .post("/api/users/colincolt")
         .send(marcData)
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(409);
           done();
         });
     });
     it("should create a file (username.json) in data directory on POST where no resource, send 201 (created)", function(done) {
       chai.request("http://localhost:8080")
-        .post("/api/user/marccolt")
+        .post("/api/users/marccolt")
         .send(marcData)
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(201);
           fs.readFile("./data/marccolt.json", function(err, data) {
-            expect(data.toString()).to.equal(JSON.stringify(marcData));
+            expect(data.toString()).to.eql(JSON.stringify(marcData));
             done();
           });
         });
@@ -128,25 +133,28 @@ describe("Persistant Server", function() {
   });
 
   describe("PUT request", function() {
-    it("should respond with 400 (bad request) on PUT where resource does not exist", function(done) {
+    it("should respond with 201 (created) on PUT where resource does not exist, create file (username.json)", function(done) {
       chai.request("http://localhost:8080")
-        .put("/api/user/erincolt")
-        .send({"username":"erincolt"})
+        .put("/api/users/garrettdeick")
+        .send({"username":"garrettdeick"})
         .end(function(err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(400);
-          done();
+          expect(err).to.eql(null);
+          expect(res).to.have.status(201);
+          fs.readFile("./data/garrettdeick.json", function(err, data) {
+            expect(data.toString()).to.eql(JSON.stringify({"username":"garrettdeick"}));
+            done();
+          });
         });
     });
     it("should respond with 200, and overwrite the resource, on PUT where resource exists", function(done) {
       chai.request("http://localhost:8080")
-        .put("/api/user/annebuch")
+        .put("/api/users/annebuch")
         .send(anneData)
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(200);
           fs.readFile("./data/annebuch.json", function(err, data) {
-            expect(data.toString()).to.equal(JSON.stringify(anneData));
+            expect(data.toString()).to.eql(JSON.stringify(anneData));
             done();
           });
         });
@@ -156,23 +164,23 @@ describe("Persistant Server", function() {
   describe("PATCH request", function() {
     it("should respond with 400 (bad request) on PATCH where resource does not exist", function(done) {
       chai.request("http://localhost:8080")
-        .patch("/api/user/erincolt")
+        .patch("/api/users/erincolt")
         .send({"emailconfirmed":true})
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(400);
           done();
         });
     });
     it("should respond with 200, and update the resource, on PATCH where resource exists", function(done) {
       chai.request("http://localhost:8080")
-        .patch("/api/user/annebuch")
+        .patch("/api/users/annebuch")
         .send({"emailconfirmed":true})
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(200);
           fs.readFile("./data/annebuch.json", function(err, data) {
-            expect(data.toString()).to.equal(JSON.stringify(anneUpdated));
+            expect(data.toString()).to.eql(JSON.stringify(anneUpdated));
             done();
           });
         });
@@ -187,21 +195,21 @@ describe("Persistant Server", function() {
     });
     it("should respond with 400 (bad request) on DELETE where resource does not exist", function(done) {
       chai.request("http://localhost:8080")
-        .del("/api/user/erincolt")
+        .del("/api/users/erincolt")
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(400);
           done();
         });
     });
     it("should delete resource, and respond with 200, on DELETE where resource exists", function(done) {
       chai.request("http://localhost:8080")
-        .del("/api/user/sarahshel")
+        .del("/api/users/sarahshel")
         .end(function(err, res) {
-          expect(err).to.be.null;
+          expect(err).to.eql(null);
           expect(res).to.have.status(200);
           fs.readFile("./data/sarahshel.json", function(err, data) {
-            expect(err.code).to.equal("ENOENT");
+            expect(err.code).to.eql("ENOENT");
             done();
           });
         });
